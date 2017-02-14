e580c74b7fc531161dbe4966de108f465178c843
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-02-15 02:21:37 +0530

    Added a title animation script

---
+++ Assets/Scripts/TitleAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleAnimation : MonoBehaviour {
+
+	[SerializeField]
+	private float duration;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<RectTransform> ().localScale = new Vector3 (Mathf.Clamp(Mathf.Abs(Mathf.Sin (Time.timeSinceLevelLoad / duration)), 0.7f, 1f), Mathf.Clamp(Mathf.Abs(Mathf.Sin (Time.timeSinceLevelLoad / duration)), 0.7f, 1f), 0f);
+	}
+}
