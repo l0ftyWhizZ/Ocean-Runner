9375c8b99fcc04139290c2915f6e905a83538d0f
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-02-15 13:09:42 +0530

    Added cases in a script to dynamically change the title colour in the main menu.

--- Assets/Scripts/TitleAnimation.cs
+++ Assets/Scripts/TitleAnimation.cs
@@ -15,5 +15,6 @@ public class TitleAnimation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		GetComponent<RectTransform> ().localScale = new Vector3 (Mathf.Clamp(Mathf.Abs(Mathf.Sin (Time.timeSinceLevelLoad / duration)), 0.7f, 1f), Mathf.Clamp(Mathf.Abs(Mathf.Sin (Time.timeSinceLevelLoad / duration)), 0.7f, 1f), 0f);
+		GetComponent<Image> ().color = new Color (Mathf.Sin (Time.timeSinceLevelLoad * duration), Mathf.Cos (Time.timeSinceLevelLoad * duration), Mathf.Sin (Time.timeSinceLevelLoad * duration), 1f);
 	}
 }
