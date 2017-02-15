5259384c170f141d83f49c1350586cf76ec0dfd5
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-02-15 13:01:56 +0530

    Added a script to randomly pick a track in the main menu as soundtrack.

---
+++ Assets/RandomAudioChooser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAudioChooser : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip[] audioClips;
+
+	private int index;
+	// Use this for initialization
+	void Awake () {
+		index = Random.Range (0, audioClips.Length);
+		GetComponent<AudioSource> ().clip = audioClips [index];
+		GetComponent<AudioSource>().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
