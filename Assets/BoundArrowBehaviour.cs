3fd5c1f29d5f34da3cbdbca1ae8d117b49045b82
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-02-27 02:04:08 +0530

    Made bounds, which have arrows flowing in direction to be gone to.

---
+++ Assets/BoundArrowBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BoundArrowBehaviour : MonoBehaviour {
+	public int scrollSpeed;
+	private int index = 0;
+	public Material mat;
+	Vector2 offset = Vector2.zero;
+
+	void Awake () {
+		mat.mainTextureOffset = Vector2.zero;
+	}
+	void Update () {
+		offset = new Vector2 (Time.time/scrollSpeed, 0f);
+
+		mat.mainTextureOffset = offset;
+	}
+}
